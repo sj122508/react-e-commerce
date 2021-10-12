35a07ab4571c31d7a7f749fc3bd89395a6295ed8
James Viojan <james.viojan@accenture.com>
2021-10-12 20:37:35 +0800

    Homepage and Directory Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import HomePage from './homePage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Diretory from "../../components/directory/directory.component";
+
+import './homePage.style.scss';
+
+const HomePage = () => {
+    return (
+    <div className='homepage'>
+        <Diretory />
+    </div>
+)
+}
+
+export default HomePage;
