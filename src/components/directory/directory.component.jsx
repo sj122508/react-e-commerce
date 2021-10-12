f8b81cf95bb755ef22255bd42fc2b8d532f851f4
James Viojan <james.viojan@accenture.com>
2021-10-12 21:33:40 +0800

    Styling Menu Items with Hover Effects

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,7 +48,12 @@ class Diretory extends Component {
         return (
             <div className='directory-menu'>
                 {this.state.sections.map(({ id, title, imageUrl, size }) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    <MenuItem
+                        key={id}
+                        title={title.toUpperCase()}
+                        imageUrl={imageUrl}
+                        size={size}
+                    />
                 ))}
             </div>
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} className={`menu-item ${size}`}>
-                <div className='content'>
-                    <h1 className='title'>{title}</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`menu-item ${size}`}>
+        <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} /> 
+        
+        <div className='content'>
+            <h1 className='title'>{title}</h1>
+            <span className='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
 )
 
 
