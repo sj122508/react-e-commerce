834eccf3492a1adc3722c12b2b2834ef6fd279f4
James Viojan <james.viojan@accenture.com>
2021-10-14 22:49:11 +0800

    Add Router , Update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch, Link } from 'react-router-dom';
+
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>HATS PAGE </h1>
+    </div>
+  )
+}
+
+const JacketsPage = () => (
+  <div>
+    <h1>JACKETS PAGE </h1>
+  </div>
+)
+
+
+const SamplePage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <Link to='/'>HomePage</Link>
+      <button onClick={() => props.history.push('/')}>HomePage</button>
+      <h1>SAMPLE PAGE : {props.match.params.Id}</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/jackets' component={JacketsPage} />
+        <Route path='/Sample/:Id' component={SamplePage} />
+      </Switch>
     </div>
   );
 }
